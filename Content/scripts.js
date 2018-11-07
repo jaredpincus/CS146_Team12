c4cfe8260fd23443dec1f578e9bda7ec7e34d931
Jared Pincus <jpincus@stevens.edu>
2018-11-07 18:20:21 -0500

    Sidebar Rework
    Completely reworked JS for sidebar and HTML setup so sidebar is procedurally made based on content of page

--- Content/scripts.js
+++ Content/scripts.js
@@ -11,3 +11,31 @@ function myFunction() {
 
   }
 }
+
+var navClass = "navLoc";
+/* Assumes that navLoc elements only go 2 levels deep */
+function generateNav() {
+	var locations = document.getElementById("content").getElementsByClassName(navClass);
+	var out = "";
+	var inner = false;
+	for (let i = 0; i < locations.length; i ++) {
+		let element = locations[i];
+		let toAdd = "<a href='#" + element.id + "'>" + element.id.replace("_"," ") + "</a>";
+		if (!inner && element.parentNode.className === navClass) {
+			inner = true;
+			out += "<ul class='dropdown'>";
+		} else if (inner && element.parentNode.className !== navClass) {
+			inner = false;
+			out += "</ul>";
+		}
+		out += toAdd;
+	};
+	console.log(out);
+	return out;
+}
+
+function placeNav() {
+	document.getElementById('sidenav').innerHTML += generateNav();
+}
+
+window.onload = placeNav;
