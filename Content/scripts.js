70802ceb0061f6cb0a1337126ce26cbf3f526627
Raj Gadhia <rgadhia@Rajs-MacBook-Pro.local>
2018-11-10 16:45:12 -0500

    Cleaned up code/Removed navbar from top of info html pages for embedding

--- Content/scripts.js
+++ Content/scripts.js
@@ -1,17 +1,3 @@
-function myFunction() {
-  var dropdowns = document.getElementsByClassName('dropdown'), i;
-
-  for (var i = 0; i < dropdowns.length; i ++) {
-
-    if(dropdowns[i].style.display === "none"){
-      dropdowns[i].style.display = "block"
-    } else {
-      dropdowns[i].style.display = "none"
-    }
-
-  }
-}
-
 var navClass = "navLoc";
 /* Assumes that navLoc elements only go 2 levels deep */
 function generateNav() {
