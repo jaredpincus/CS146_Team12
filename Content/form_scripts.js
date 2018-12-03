8697aa51d7ac846c8b61dbbba677ee73146bba14
Raj Gadhia <rgadhia@Rajs-MacBook-Pro.local>
2018-12-02 19:36:54 -0500

    Event listeners - picture changes according to which button you choose

---
+++ Content/form_scripts.js
@@ -0,0 +1,71 @@
+var navClass = "navLoc";
+/* Assumes that navLoc elements only go 2 levels deep */
+function generateNav() {
+	var locations = document.getElementById("content").getElementsByClassName(navClass);
+	var out = "";
+	var inner = false;
+	for (let i = 0; i < locations.length; i ++) {
+		let element = locations[i];
+		let spanStart = element.parentNode.className !== navClass ? "<span class='navGroup'>" : "";
+		let toAdd = spanStart + "<a href='#" + element.id + "'>" + element.id.split("_").join(" ") + "</a>";
+		if (!inner && element.parentNode.className === navClass) {
+			inner = true;
+			out += "<ul class='dropdown'>";
+		} else if (inner && element.parentNode.className !== navClass) {
+			inner = false;
+			out += "</ul></span>";
+		}
+		out += toAdd;
+	};
+	console.log(out);
+	return out;
+}
+
+function placeNav() {
+	document.getElementById('sidenav').innerHTML += generateNav();
+}
+
+window.onload = placeNav;
+
+window.addEventListener("load", function() {
+  var cripplingbutton = document.getElementById("crippling");
+  var thanksbutton = document.getElementById("no thanks");
+  var aightbutton = document.getElementById("it was aight");
+  var gerdbutton = document.getElementById("really gerd");
+  var lifebutton = document.getElementById("life changing");
+  var rating = document.getElementById("rating_pic")
+
+  // Get the modal
+  var modal = document.getElementById('myModal');
+
+  // Get the button that opens the modal
+  var btn = document.getElementById("submitbutton");
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  cripplingbutton.addEventListener("click", function() {
+    rating.src = "was crippling.png"
+  });
+  thanksbutton.addEventListener("click", function() {
+    rating.src = "no thanks.jpg"
+  });
+  aightbutton.addEventListener("click", function() {
+    rating.src = "it was aight.png"
+  });
+  gerdbutton.addEventListener("click", function() {
+    rating.src = "really good.jpeg"
+  });
+  lifebutton.addEventListener("click", function() {
+    rating.src = "life changing.jpeg"
+  });
+
+  btn.addEventListener("click", function() {
+    modal.style.display = "block";
+  })
+
+  span.addEventListener("click", function() {
+    modal.style.display = "none";
+  })
+
+});
